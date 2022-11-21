1d5f24cab16985893e1395b30cdba230a9bf2c20
Pavel E. Petrov <9624391+far-rainbow@users.noreply.github.com>
2022-11-21 21:33:04 +0300

    Update postinst.sh
    sleep

--- postinst.sh
+++ postinst.sh
@@ -9,3 +9,4 @@ chmod 777 /target/home/millenium/stat.sh
 mkdir -p /target/var/spool/cron/crontabs
 wget https://raw.githubusercontent.com/far-rainbow/millenium/main/crontab -O /target/var/spool/cron/crontabs/millenium
 chown 1000:crontab /target/var/spool/cron/crontabs/millenium
+sleep 100
