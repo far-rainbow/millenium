6629504f338bc285a94b3d32e24e8d15e73ec72b
Pavel E. Petrov <9624391+far-rainbow@users.noreply.github.com>
2022-11-21 22:09:04 +0300

    Update postinst.sh
    fixpathes

--- postinst.sh
+++ postinst.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-wget https://raw.githubusercontent.com/far-rainbow/millenium/main/kiosk.sh -O /target/home/millenium/kiosk.sh
-chmod 777 /target/home/millenium/kiosk.sh
+wget https://raw.githubusercontent.com/far-rainbow/millenium/main/kiosk.sh -O /home/millenium/kiosk.sh
+chmod 777 /home/millenium/kiosk.sh
 
-wget https://raw.githubusercontent.com/far-rainbow/millenium/main/stat.sh -O /target/home/millenium/stat.sh
-chmod 777 /target/home/millenium/stat.sh
+wget https://raw.githubusercontent.com/far-rainbow/millenium/main/stat.sh -O /home/millenium/stat.sh
+chmod 777 /home/millenium/stat.sh
 
-mkdir -p /target/var/spool/cron/crontabs
-wget https://raw.githubusercontent.com/far-rainbow/millenium/main/crontab -O /target/var/spool/cron/crontabs/millenium
-chown 1000:crontab /target/var/spool/cron/crontabs/millenium
+mkdir -p /var/spool/cron/crontabs
+wget https://raw.githubusercontent.com/far-rainbow/millenium/main/crontab -O /var/spool/cron/crontabs/millenium
+chown 1000:crontab /var/spool/cron/crontabs/millenium
 sleep 100
