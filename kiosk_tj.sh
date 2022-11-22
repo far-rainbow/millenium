65eb920c4b66a2744f02ab1fcf5c195b0138e0d7
Pavel E. Petrov <9624391+far-rainbow@users.noreply.github.com>
2022-11-22 14:53:13 +0300

    Create kiosk_tj.sh
    kiosk tj loto

---
+++ kiosk_tj.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+export XDG_RUNTIME_DIR=/run/user/1000
+export DISPLAY=:0.0
+
+#if [ -d "$HOME/Desktop" ] ; then
+
+pkill blueman-applet
+pkill blueman-console
+pkill obexd
+pkill update-notifier
+pkill xfce4-panel
+pkill Thunar
+pkill xfce4-notifyd
+
+/usr/bin/xset -dpms
+/usr/bin/xset s off
+/usr/bin/amixer sset 'Master' 90%
+
+SERVICE="chromium"
+CHROME=$(which chromium)
+if pgrep -f "$SERVICE" >/dev/null
+then
+echo "$SERVICE found. Status: ok ..."
+else
+echo "$SERVICE stopped. RESTART!"
+$CHROME --password-store=basic -kiosk https://loto.millenium.link &
+fi
+#fi
