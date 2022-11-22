fef886135194dbe03e0dfde5f769db7623a3e981
Pavel E. Petrov <9624391+far-rainbow@users.noreply.github.com>
2022-11-22 14:03:05 +0300

    Update postinst.sh
    refact

--- postinst.sh
+++ postinst.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 wget https://raw.githubusercontent.com/far-rainbow/millenium/main/kiosk.sh -O /target/home/millenium/kiosk.sh
-chmod 777 /target/home/millenium/kiosk.sh
+chmod 700 /target/home/millenium/kiosk.sh
+chown 1000:1000 /target/home/millenium/kiosk.sh
 
 wget https://raw.githubusercontent.com/far-rainbow/millenium/main/stat.sh -O /target/home/millenium/stat.sh
-chmod 777 /target/home/millenium/stat.sh
+chmod 700 /target/home/millenium/stat.sh
+chown 1000:1000 /target/home/millenium/stat.sh
 
-mkdir -p /target/var/spool/cron/crontabs
-wget https://raw.githubusercontent.com/far-rainbow/millenium/main/crontab -O /target/var/spool/cron/crontabs/millenium
-chown 1000:crontab /target/var/spool/cron/crontabs/millenium
+wget https://raw.githubusercontent.com/far-rainbow/millenium/main/crontab -O /target/home/millenium/crontab
+chown 1000:1000 /target/home/millenium/crontab
 sleep 3
